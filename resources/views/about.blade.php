f002d3ab8e2b718733514c1b0f8cb5cfc927e382
brunoborre1 <brunoborrello@gmail.com>
2020-10-27 00:32:44 -0300

    creo vistas

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+
+@section('content')
+    <h1>About</h1>
+@stop
+
+
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,479 @@
+<!Doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<!-- TITLE OF SITE -->
+		<title>Hecho con Amor</title>
+		
+        <!-- responsive meta tag -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<!--font-family-->
+		<link href='https://fonts.googleapis.com/css?family=Lobster+Two:400,700,400italic' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+		
+		<!--base.css-->
+		<link rel="stylesheet" href="css/base.css"  type="text/css"/>
+		<!--hover.min.css-->
+		<link href="css/hover-min.css" rel="stylesheet" type="text/css"/>
+		
+		<!--font-awesome.min.css-->
+		<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+		
+		<!--SLIDER REVOLUTION 4.x CSS SETTINGS-->
+		<link href="plugin/css/captions.css" rel="stylesheet" type="text/css"/>
+		<link href="plugin/css/settings.css" rel="stylesheet" type="text/css"/>
+		
+		<!-- slicknav.css -->
+		<link href="css/slicknav.css" rel="stylesheet" type="text/css"/>
+		
+		<!--prettyPhoto CSS-->
+		<link href="css/prettyPhoto.css" rel="stylesheet" type="text/css"/>
+		
+		<!--bootstrap.min.css-->
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+		
+		<!--style.css-->
+		<link href="css/preset.css" rel="stylesheet" type="text/css"/>
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		
+		<!--responsive.css-->
+		<link href="css/responsive.css" rel="stylesheet" type="text/css"/>
+		
+	</head>
+	
+	<body>
+	<!-- Start header -->
+		<section id="header">
+			<div class="logo">
+				<a href="index.html"><img src="images/red-logo.png" alt="image"/></a>
+			</div>
+			<div class="menu-top">
+				<div class="logo-bottom"></div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<nav class="navbar navbar-default">
+							<div class="container-fluid">
+								<!-- Collect the nav links, forms, and other content for toggling -->
+								<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+									<ul class="nav navbar-nav">
+										<li class="dropdown">
+										  <a href="index.html">Home</a>
+										 <ul class="dropdown-menu">
+											<li><a href="http://themelan.com/html/redbowl/">Redbowl Red</a></li>
+											<li><a href="http://themelan.com/html/redbowl/blue/">Redbowl Blue</a></li>
+											<li><a href="http://themelan.com/html/redbowl/brown/">Redbowl Brown</a></li>
+											<li><a href="http://themelan.com/html/redbowl/olive/">Redbowl Olive</a></li>
+										  </ul>
+										</li>
+										<li class="dropdown">
+										  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
+										  <ul class="dropdown-menu">
+											<li><a href="gallery.html">Gallery</a></li>
+											<li class="dropdown">
+												<a href="blog.html" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" 	aria-expanded="false">Blog</a>
+												<ul class="sub-menu">
+													<li class="child"><a href="categories.html">Categories</a></li>
+													<li class="child"><a href="tag.html">Tag pages</a></li>
+													<li class="child"><a href="super-user.html">User Page</a></li>
+													<li class="child"><a href="single.html">Single Page</a></li>
+												</ul>
+											</li>
+											<li><a href="member-login.html">Member Login</a></li>
+											<li><a href="registration.html">Registration</a></li>
+											<li><a href="404page.html">404 Page</a></li>
+										  </ul>
+										</li>
+										
+										<li class="dropdown">
+										  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Food Menu</a>
+										  <ul class="dropdown-menu">
+											<li><a href="food-menu.html">Lunch Special and Salad</a></li>
+											<li><a href="food-menu2.html">Soup,Stew,Casserole</a></li>
+											<li><a href="food-menu3.html">Redbowl Special Menu</a></li>
+										  </ul>
+										</li>
+										<li><a href="about-us.html">About Us</a></li>
+										<li><a href="our-chefs.html">Our Chefs</a></li>
+										<li><a href="online-order.html">Order Online</a></li>
+										<li><a href="reservation.html">Resarvation</a></li>
+										<li><a href="contact-us.html">Contact Us</a></li>
+									</ul>
+								</div><!-- /.navbar-collapse -->
+							</div><!-- /.container-fluid -->
+						</nav><!-- /.navbar navbar-default -->
+					</div><!-- /.coloum -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- #header -->
+		<!-- End header -->
+		
+		
+		<!--Start revolution-slider-->
+		<section id="revolution">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<!-- START REVOLUTION SLIDER ver. 2.2.4 -->
+						<div class="rev_slider_wrapper">
+							<div id="rev_slider_1_1" class="rev_slider" style="display:none;">
+								<ul>
+									<li data-transition="fade" data-slotamount="7" data-masterspeed="900" >
+										<img src="http://placehold.it/922x385" alt="image" />
+										<div class="tp-caption lfl"
+											 data-x="-19"
+											 data-y="408"
+											 data-speed="300"
+											 data-start="500"
+											 data-easing="easeInSine"  ><img src="images/slider/glow.png" alt="Image 1"></div>
+										<div class="tp-caption very_large_text lft"
+											 data-x="48"
+											 data-y="261"
+											 data-speed="800"
+											 data-start="1700"
+											 data-easing="easeOutBounce"  >Delicious</div>
+										<div class="tp-caption medium_text lfr"
+											 data-x="41"
+											 data-y="441"
+											 data-speed="300"
+											 data-start="500"
+											 data-easing="easeInSine"  >Serving quality foods that will make you remember coming back to us ! MORE & MORE !!</div>
+										<div class="tp-caption randomrotate"
+											 data-x="42"
+											 data-y="350"
+											 data-speed="300"
+											 data-start="1100"
+											 data-easing="easeInSine"  ><img src="images/slider/L1.png" alt="Image 4"></div>
+										<div class="tp-caption randomrotate"
+											 data-x="42"
+											 data-y="359"
+											 data-speed="300"
+											 data-start="1400"
+											 data-easing="easeInSine"  ><img src="images/slider/L2.png" alt="Image 5"></div>
+									</li>
+									<li data-transition="fade" data-slotamount="7" data-masterspeed="900" >
+										<img src="http://placehold.it/922x385" alt="image" />
+										<div class="tp-caption lfl"
+											 data-x="-19"
+											 data-y="408"
+											 data-speed="300"
+											 data-start="500"
+											 data-easing="easeInSine"  ><img src="images/slider/glow.png" alt="Image 1"></div>
+										<div class="tp-caption very_large_text lft"
+											 data-x="48"
+											 data-y="261"
+											 data-speed="800"
+											 data-start="1700"
+											 data-easing="easeOutBounce"  >Yummmi</div>
+										<div class="tp-caption medium_text lfr"
+											 data-x="41"
+											 data-y="441"
+											 data-speed="300"
+											 data-start="500"
+											 data-easing="easeInSine"  >Serving quality foods that will make you remember coming back to us ! MORE & MORE !!</div>
+										<div class="tp-caption randomrotate"
+											 data-x="42"
+											 data-y="350"
+											 data-speed="300"
+											 data-start="1100"
+											 data-easing="easeInSine"  ><img src="images/slider/L1.png" alt="Image 4"></div>
+										<div class="tp-caption randomrotate"
+											 data-x="42"
+											 data-y="359"
+											 data-speed="300"
+											 data-start="1400"
+											 data-easing="easeInSine"  ><img src="images/slider/L2.png" alt="Image 5"></div>
+									</li>
+									<li data-transition="fade" data-slotamount="7" data-masterspeed="900" >
+										<img src="http://placehold.it/922x385" alt="image" />
+										<div class="tp-caption lfl"
+											 data-x="-19"
+											 data-y="408"
+											 data-speed="300"
+											 data-start="500"
+											 data-easing="easeInSine"  ><img src="images/slider/glow.png" alt="Image 1"></div>
+										<div class="tp-caption very_large_text lft"
+											 data-x="48"
+											 data-y="261"
+											 data-speed="800"
+											 data-start="1700"
+											 data-easing="easeOutBounce"  >Fabolous</div>
+										<div class="tp-caption medium_text lfr"
+											 data-x="41"
+											 data-y="441"
+											 data-speed="300"
+											 data-start="500"
+											 data-easing="easeInSine"  >Serving quality foods that will make you remember coming back to us ! MORE & MORE !!</div>
+										<div class="tp-caption randomrotate"
+											 data-x="42"
+											 data-y="350"
+											 data-speed="300"
+											 data-start="1100"
+											 data-easing="easeInSine"  ><img src="images/slider/L1.png" alt="Image 4"></div>
+										<div class="tp-caption randomrotate"
+											 data-x="42"
+											 data-y="359"
+											 data-speed="300"
+											 data-start="1400"
+											 data-easing="easeInSine"  ><img src="images/slider/L2.png" alt="Image 5"></div>
+									</li>
+								</ul>
+								<div class="tp-bannertimer"></div>
+							</div>
+						</div>
+						<div class="clear"></div>
+					</div><!-- /.coloum -->
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</section><!-- /#revolution -->
+		<!-- END REVOLUTION SLIDER -->
+		
+		
+		
+		<!-- Start slider-bottom-->
+		<section id="slider-bottom">
+			<div class="container">
+				<div class="banner">
+                    <img src="images/banner3.png"  alt="image" />
+                    <p>Welcome to Redbowl Restaurant</p>
+                </div>
+			</div><!-- .container-->
+		</section><!-- #slider-bottom-->
+		<!-- end slider-bottom -->
+		
+		
+		
+		<!-- Start module-->
+		<section id="module">
+			<div class="container">
+				<div class="main-body-top">
+					<div class="main-body-top-sitebar"></div>
+					<div class="main-body-top-mid"></div>
+					<div class="main-body-top-sitebar"></div>
+				</div>
+				<div class="module-box pp">
+					<div class="wrapper-title">
+						<h3>Serving quality foods that will make you remember coming back to us</h3>
+					</div>
+					<div id="feature" class="row ">
+						<div class="col-md-4 col-sm-4">
+							<div class="content-box ">
+								<h2>Our Story</h2>
+								<div class="img effects clearfix">
+									<a href="images/f1.jpg"><img src="http://placehold.it/550x297"  alt="image"/></a>
+									<div class="overlay">
+										<a  href="http://placehold.it/550x297" rel="prettyPhoto[pp]" class="expand">+</a>
+										<a class="close-overlay hidden">x</a>
+									</div>
+								</div>
+								<p>Serving as a kitchen cook for the royal court cuisine of the Joseon Dynasty, Jang Geum aspired to attain the secrets of Korean cooking. She was later appointed as the first female royal physician to the King, and she applied the culinary and medicinal secrets that she discovered to the cure of the King Jung Jong's illness.</p>
+								<a href="#" class="button btn btn-primary hvr-ripple-out">More...</a>
+							</div>
+						</div>
+						
+						<div class="col-md-4 col-sm-4">
+							<div class="content-box">
+								<h2>Beautiful Dishes</h2>
+								<div class="img effects clearfix">
+									<a href="images/f2.png"><img src="http://placehold.it/550x297"  alt="image"/></a>
+									<div class="overlay">
+										<a  href="http://placehold.it/550x297" rel="prettyPhoto[pp]" class="expand">+</a>
+										<a class="close-overlay hidden">x</a>
+									</div>
+								</div>
+								<p>Serving as a kitchen cook for the royal court cuisine of the Joseon Dynasty, Jang Geum aspired to attain the secrets of Korean cooking. She was later appointed as the first female royal physician to the King, and she applied the culinary and medicinal secrets that she discovered to the cure of the King Jung Jong's illness.</p>
+								<a href="#" class="button btn btn-primary hvr-ripple-out">More...</a>
+							</div>
+						</div>
+						
+						<div class="col-md-4 col-sm-4">
+							<div class="content-box">
+								<h2>Lovely Atmosphere</h2>
+								<div class="img effects clearfix">
+									<a href="images/f3.jpg"><img src="http://placehold.it/550x297"  alt="image"/></a>
+									<div class="overlay">
+										<a  href="http://placehold.it/550x297" rel="prettyPhoto[pp]" class="expand">+</a>
+										<a class="close-overlay hidden">x</a>
+									</div>
+								</div>
+								<p>Serving as a kitchen cook for the royal court cuisine of the Joseon Dynasty, Jang Geum aspired to attain the secrets of Korean cooking. She was later appointed as the first female royal physician to the King, and she applied the culinary and medicinal secrets that she discovered to the cure of the King Jung Jong's illness.</p>
+								<a href="#" class="button btn btn-primary hvr-ripple-out">More...</a>
+							</div>
+						</div>
+					</div>
+					
+					<div class="slider">
+						<div class="row">
+							<div class="col-md-12">
+								<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+									<!-- Wrapper for slides -->
+									<div class="carousel-inner" role="listbox">
+										<div class="item active">
+											<img src="http://placehold.it/91x91" alt="image">
+											<div class="s-content">	
+												<p>Not only is the restaurant filled with a fantastic atmosphere, with friendly staff and beautiful decor, the food is the restaurant filled with a fantastic atmosphere, with friendly absolut...</p>
+												<h4>- - - Scarlett Johansson - - -</h4>
+											</div>
+										</div>
+										
+										<div class="item">
+											<img src="http://placehold.it/91x91" alt="image">
+											<div class="s-content">
+												<p>Not only is the restaurant filled with a fantastic atmosphere, with friendly staff and beautiful decor, the food is the restaurant filled with a fantastic atmosphere, with friendly absolut...</p>
+												<h4>- - - Scarlett Johansson - - -</h4>
+											</div>
+										</div>
+										
+										<div class="item">
+											<img src="http://placehold.it/91x91" alt="image">
+											<div class="s-content">
+												<p>Not only is the restaurant filled with a fantastic atmosphere, with friendly staff and beautiful decor, the food is the restaurant filled with a fantastic atmosphere, with friendly absolut...</p>
+												<h4>- - - Scarlett Johansson - - -</h4>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div><!--.row-->
+					</div><!--.slider-->
+                </div><!--.module-box-->
+				<div class="shadow">
+					<img src="images/shadow.png" alt="image">
+				</div>
+			</div><!--.container-->
+		</section><!--#module-->
+		<!-- End Moudule-->
+	
+	
+		<!--start bottom-->
+		<section id="bottom">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 col-sm-4 col-lg-4">
+						<div class="bottom-box">
+							<h3>Opening Hours</h3>
+							<h4>11:45AM - 3PM to 6PM - 11PM</h4>
+						</div>
+					</div>
+					
+					<div class="col-md-4 col-sm-4 col-lg-4">
+						<div class="bottom-box">
+							<h3>Resarvation</h3>
+							<a href="reservation.html"><p><span>Resarvation For  </span>Click Here</p></a>
+						</div>
+					</div>
+					
+					<div class="col-md-4 col-sm-4 col-lg-4">
+						<div class="bottom-box">
+							<h3>Redbowl Hotline</h3>
+							<p>123456789, 05-8837681</p>
+						</div>
+					</div>
+				</div><!--.row-->
+			</div><!--.container-->
+		</section><!--#bottom-->
+		<!--end bottom-->
+	
+	
+	
+		<!--footer start-->
+		<section id="footer" class="text-center">
+			<div class="container">
+				<div class="footer-content">
+					<h4>REDBOWL RESTAURANT, Block-69/B, Road-30, West Dowtown, Miami, Florida</h4>
+					<p>Redbowl Restaurant by <span>Themelan</span>, Built on Bootstrap Framework</p>
+				</div>
+				
+				<div class="social-icon">
+					<ul class="ch-grid">
+						<li>
+							<div class="ch-item ch-img-1">
+								<div class="ch-info">
+									<p><a href="#">Share<br>It</a></p>
+								</div>
+								<div class="ch-thumb"><a href="#"><i class="fa fa-facebook"></i></a></div>
+							</div>
+						</li>
+						<li>
+							<div class="ch-item ch-img-1">
+								<div class="ch-info">
+									<p><a href="#">Share<br>It</a></p>
+								</div>
+								<div class="ch-thumb"><a href="#"><i class="fa fa-twitter"></i></a></div>
+							</div>
+						</li>
+						<li>
+							<div class="ch-item ch-img-1">
+								<div class="ch-info">
+									<p><a href="#">Share<br>It</a></p>
+								</div>
+								<div class="ch-thumb"><a href="#"><i class="fa fa-youtube"></i></a></div>
+							</div>
+						</li>
+						<li>
+							<div class="ch-item ch-img-1">
+								<div class="ch-info">
+									<p><a href="#">Share<br>It</a></p>
+								</div>
+								<div class="ch-thumb"><a href="#"><i class="fa fa-pinterest"></i></a></div>
+							</div>
+						</li>
+						<li>
+							<div class="ch-item ch-img-1">
+								<div class="ch-info">
+									<p><a href="#">Share<br>It</a></p>
+								</div>
+								<div class="ch-thumb"><a href="#"><i class="fa fa-wifi"></i></a></div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div><!--.container-->
+			
+			<div class="scrollup">
+				<a href="">Top</a>
+				<img src="images/red-top.png" alt="image"/>
+			</div>	
+		</section><!--#footer-->
+		<!--end footer-->
+	
+	
+	
+		<!--jquery,bootstrap,modernizr.js-->
+		<script src="js/modernizr.js" type="text/javascript"></script>
+		<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+		<script src="js/bootstrap.min.js" type="text/javascript"></script>
+		
+		<!--SLIDER REVOLUTION 4.x SCRIPTS-->
+		<script type="text/javascript" src="plugin/js/jquery.themepunch.plugins.min.js"></script>
+		<script type="text/javascript" src="plugin/js/jquery.themepunch.revolution.min.js"></script>	
+		<script type="text/javascript" src="plugin/js/revolution.callback.js"></script>
+		
+		<!--jquery.scrollUp.js-->
+		<script src="js/jquery.scrollUp.min.js" type="text/javascript"></script>
+		
+		<!-- slicknav.js -->
+		<script src="js/jquery.slicknav.js" type="text/javascript"></script>
+		
+		<!--prettyPhoto.js-->
+		<script src="js/jquery.prettyPhoto.js" type="text/javascript"></script>
+		
+		<!--custom.js-->
+		<script src="js/custom.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			// prettyPhoto Script
+			$("area[rel^='prettyPhoto']").prettyPhoto();
+			
+			$(".pp:first a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:3000, autoplay_slideshow: false});
+			$(".pp:gt(0) a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'fast',slideshow:10000, hideflash: true});
+		
+		</script>
+	</body>
+
+    @yield('content')
+    
+</html>
+
